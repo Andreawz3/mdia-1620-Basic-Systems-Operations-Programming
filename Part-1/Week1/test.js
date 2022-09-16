b815a10a4f7c0043e6baf9897b5e03c4c2eaab0d
Andrea Wu Zheng <awuzheng@my.bcit.ca>
2022-09-16 12:16:26 -0700

    adding more code

---
+++ Part-1/Week1/test.js
@@ -0,0 +1,17 @@
+// Data Type + Object
+/**
+ * 6-4-1 create a variable called grannySmith and add the 
+ * following values (use camelCase)
+ * colour of the apple is green
+ * origin location is Autralia
+ * origin date is 1868
+ * then console.log  from the colour from the object
+ */ 
+
+ var grannySmith = {
+    colour: "green",
+    originLocation: "australia",
+    originDate: 1868
+};
+
+console.log(grannySmith);
